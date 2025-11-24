53790c6291436426593e668e48e8b221355ed261
Vuk Sekulic <vukosek@gmail.com>
2025-11-24 20:22:18 +0100

    Patch: Followers dont authenticate anymore

--- FollowerService/src/Explorer.API/Controllers/FollowersController.cs
+++ FollowerService/src/Explorer.API/Controllers/FollowersController.cs
@@ -1,4 +1,5 @@
 ﻿using Explorer.Stakeholders.API.Public;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,7 +7,6 @@ namespace Explorer.API.Controllers
 {
     [ApiController]
     [Route("api/followers")]
-    [Authorize]
     public class FollowersController : ControllerBase
     {
         private readonly IFollowersService _service;
@@ -17,41 +17,36 @@ namespace Explorer.API.Controllers
         }
 
         [HttpPost("{targetId}")]
-        public async Task<IActionResult> Follow(long targetId)
+        public async Task<IActionResult> Follow(long targetId, [FromQuery] long userId)
         {
-            long userId = long.Parse(User.FindFirst("sub")!.Value);
             await _service.Follow(userId, targetId);
             return Ok();
         }
 
         [HttpDelete("{targetId}")]
-        public async Task<IActionResult> Unfollow(long targetId)
+        public async Task<IActionResult> Unfollow(long targetId, [FromQuery] long userId)
         {
-            long userId = long.Parse(User.FindFirst("sub")!.Value);
             await _service.Unfollow(userId, targetId);
             return Ok();
         }
 
         [HttpGet("me/followers")]
-        public async Task<IActionResult> GetMyFollowers()
+        public async Task<IActionResult> GetMyFollowers([FromQuery] long userId)
         {
-            long userId = long.Parse(User.FindFirst("sub")!.Value);
             var followers = await _service.GetFollowers(userId);
             return Ok(followers);
         }
 
         [HttpGet("me/following")]
-        public async Task<IActionResult> GetMyFollowing()
+        public async Task<IActionResult> GetMyFollowing([FromQuery] long userId)
         {
-            long userId = long.Parse(User.FindFirst("sub")!.Value);
             var following = await _service.GetFollowing(userId);
             return Ok(following);
         }
 
         [HttpGet("suggested")]
-        public async Task<IActionResult> Suggested()
+        public async Task<IActionResult> Suggested([FromQuery] long userId)
         {
-            long userId = long.Parse(User.FindFirst("sub")!.Value);
             var suggestions = await _service.GetSuggestedFollowers(userId);
             return Ok(suggestions);
         }
